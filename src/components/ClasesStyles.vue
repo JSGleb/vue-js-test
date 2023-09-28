d22c1538d959c1b4aaa55b128f0009513f6d335f
JSGleb <gkiryakov.jst@gmail.com>
2023-09-28 14:27:57 +0300

    Сделаны классовые стили

---
+++ src/components/ClasesStyles.vue
@@ -0,0 +1,64 @@
+<template>
+    <!-- <div :class="{ 'active': isActive, 'disabled': isDisabled }">Классы стилей</div> -->
+    <div>
+        <button @click="switchBtn">Switcher</button>
+        <button :class="btnClass">{{ btnText }}</button>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'ClasesStyles',
+    data() {
+        return {
+            isActive: true,
+            isDisabled: false,
+            btnText: 'Click on me!'
+        }
+    },
+
+    methods: {
+        switchBtn() {
+            this.isActive = !this.isActive;
+            this.isDisabled = !this.isDisabled;
+            console.log(this.isActive)
+            console.log(this.isDisabled)
+        }
+    },
+
+    computed: {
+        btnClass() {
+            return {
+                'active': this.isActive,
+                'disabled': this.isDisabled
+            }
+        }
+    }
+}
+
+</script>
+
+<style scoped>
+
+    button:nth-of-type(2) {
+        font-size: 2em;
+        width: 100%;
+        padding: 20px;
+    }
+    .active {
+        background-color: darkcyan;
+        color: yellowgreen;
+        cursor: pointer;
+        transition: all .4s ease-in;
+    }
+    .active:hover {
+        background-color: red;
+    }
+
+    .disabled {
+        opacity: .5;
+        padding: 20px;
+        cursor: not-allowed;
+    }
+</style>
